e4adef51ff82c9707f46fbf60778f259feaa9ca6
DOUFARE Jawad <Jawad.DOUFARE@cegedim.com>
2022-07-27 20:12:37 +0100

    ADDING fEEDBACK STATS COMPONNENT

---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function FeedbackStats(props) {
+
+   const n = props.feedback.reduce((totalCurr,feed) => {
+    console.log(totalCurr);    
+    return feed.rating + totalCurr;
+  },0) / props.feedback.length.toFixed(1)
+  return (
+    <div className='feedback-stats'>
+      <h4>{props.feedback.length} Reviews</h4>
+      <h4>Average Rating: { n }</h4>
+    </div>
+   
+  )
+}
+
+export default FeedbackStats
