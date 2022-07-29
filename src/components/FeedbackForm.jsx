dd7250b438e56a2ef4539af114b1a58327cba800
DOUFARE Jawad <Jawad.DOUFARE@cegedim.com>
2022-07-29 10:05:43 +0100

    adding form componenet2

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,7 +3,19 @@ import Card from './shared/Card'
 import Button from './shared/button';
 function FeedbackForm (){
  const [text,setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
  const handleTextChange = (e) => {
+    if (text === '') {
+        setBtnDisabled(true)
+        setMessage(null)
+      } else if (text !== '' && text.trim().length <= 10) {
+        setMessage('Text must be at least 10 characters')
+        setBtnDisabled(true)
+      } else {
+        setMessage(null)
+        setBtnDisabled(false)
+      }
     setText(e.target.value);
  }
 return (
@@ -19,8 +31,11 @@ return (
           placeholder='Write a review'
           value={text}
         />
-         <Button type='submit'>Send</Button> 
+       
+         <Button type='submit' isDisabled={btnDisabled}>Send</Button> 
+          
       </div>
+      { message }
     </form>
   </Card>
 )
